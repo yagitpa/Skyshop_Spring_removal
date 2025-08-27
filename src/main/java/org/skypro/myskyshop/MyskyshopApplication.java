95cde0ab4bdba22343257ca097f4872f5fe53137
yagitpa <yakov.palchikov@gmail.com>
2025-08-27 18:58:13 +0300

    Start my Skyshop removal

---
+++ src/main/java/org/skypro/myskyshop/MyskyshopApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.myskyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyskyshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyskyshopApplication.class, args);
+	}
+
+}
